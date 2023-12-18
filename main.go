fb1d5137c5a57ffa11e08c378530e7f821acaf99
Syed Khaja Fareeduddin <skfareeduddin5@gmail.com>
2023-12-18 20:15:50 +0530

    Added the functionality to generate cards.

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Six of Spades", newCard()}
-	cards = append(cards, "Ace of Spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
